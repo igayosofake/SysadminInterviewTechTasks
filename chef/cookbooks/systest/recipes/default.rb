dbabf39730bafa74aa6230287af9e2fecf405044
igayosofake <igayoso+fake@gmail.com>
2015-01-15 23:10:27 +0000

    Add sudo cookbook

--- chef/cookbooks/systest/recipes/default.rb
+++ chef/cookbooks/systest/recipes/default.rb
@@ -6,3 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+user "blogger" do
+  supports :manage_home => true
+  comment "blogger User"
+  gid "users"
+  home "/home/blogger"
+  shell "/bin/bash"
+  password "$1$mJ6aTqJG$KJJ1r4dMdZ9M7KRgqiP.Y/"
+end
